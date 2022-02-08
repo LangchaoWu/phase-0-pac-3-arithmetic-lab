ffe212f7984c960d2a3b6bc0aecc31c41999a4b6
Langchao Wu <langchaowu@yahoo.com>
2022-02-07 22:37:25 -0500

    commit tree 98f7bf6bcd2f7cb481556ac8de5613b1574afd12

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(number){
+    return number++;
+}
+
+function subtract(number){
+    return number--;
+}
+
+function multiply(number){
+    return number *= 10 ;
+}
+
+function divide(number){
+    return number /=3;
+}
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function increment(number){
+    return ++number;
+}
+
+function decrement(number){
+    return --number;
+}
+
+function makeInt(n){
+    return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
